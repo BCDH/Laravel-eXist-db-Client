e6af4a03af198660052ad7176c97e46dbbb8dbf8
zilbe <zilbertrud@gmail.com>
2016-04-19 07:07:40 +0000

    - package renamed

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -1,5 +1,11 @@
 <?php namespace BCDH\ExistDbClient;
 
+/**
+ * Please update include-path in composer.json to `vendor/pear/xml_rpc2/` and change admin credentials in this class in order to run TestUnit
+ *
+ * Class ClientTest
+ * @package BCDH\ExistDbClient
+ */
 class ClientTest extends \PHPUnit_Framework_TestCase
 {
     /**
